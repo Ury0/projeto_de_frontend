4244aa63e34340474f62fdaf185e27b3ca2c1c51
Ury <117782130+Ury0@users.noreply.github.com>
2024-10-30 00:13:23 -0300

    css

--- trabalho-marlon/src/app/componentes/main/home/home.component.ts
+++ trabalho-marlon/src/app/componentes/main/home/home.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  showRendaFixa = false;
+  showRendaVariavel = false;
 
+  toggleContent(section: string): void {
+    if (section === 'rendaFixa') {
+      this.showRendaFixa = !this.showRendaFixa;
+      if (this.showRendaVariavel) this.showRendaVariavel = false;
+    } else if (section === 'rendaVariavel') {
+      this.showRendaVariavel = !this.showRendaVariavel;
+      if (this.showRendaFixa) this.showRendaFixa = false;
+    }
+  }
 }
+
