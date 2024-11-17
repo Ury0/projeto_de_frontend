cab920aea31a0724e4160304caf578211e2827b4
Ury <117782130+Ury0@users.noreply.github.com>
2024-11-17 14:58:05 -0300

    fix

--- trabalho-marlon/src/app/app-routing.module.ts
+++ trabalho-marlon/src/app/app-routing.module.ts
@@ -6,33 +6,14 @@ import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { CarteiraComponent } from './paginas/carteira/carteira.component';
 import { ListagemComponent } from './paginas/listagem/listagem.component';
 import { DashboardComponent } from './paginas/dashboard/dashboard.component';
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-
-
-
-const routes: Routes = [
-=======
 import { IndexComponent } from './paginas/logIN/index.component';
 import { CadastroComponent } from './paginas/cadastro/cadastro.component';
-import path from 'path';
 
 
 const routes: Routes = [
   {path: 'home', component: IndexComponent},
   {path: 'cadastro', component: CadastroComponent},
->>>>>>> Stashed changes
   {path: '', component: HomeComponent},
-=======
-import { IndexComponent } from './paginas/logIN/index.component';
-import { CadastroComponent } from './paginas/cadastro/cadastro.component';
-
-
-const routes: Routes = [
-  {path: '', component: IndexComponent},
-  {path: 'cadastro', component: CadastroComponent},
-  {path: 'home', component: HomeComponent},
->>>>>>> 69c0be0025cd8492d440fa4e2c2edd4f74e4f728
   {path:'perfil', component: PerfilComponent},
   {path: 'carteira', component: CarteiraComponent},
   {path: 'listagem', component: ListagemComponent},
